b96f03748df9cc4680404593303b86c6d6f8c7a2
Jani Heikkila <t1heja00@students.oamk.fi>
2022-09-22 20:48:33 +0300

    projekti alustettu ja book controlleri alustettu

---
+++ models/book_model.js
@@ -0,0 +1,10 @@
+const book = {
+    getAllBooks: function(id, callback) {
+        return "All Books";
+    },
+    getOneBook:function(id){
+        return "Book which id="+id;
+    }
+}
+
+module.exports=book;
